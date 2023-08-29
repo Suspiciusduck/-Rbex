7320299739007c0c8212d74813e5ba10a54882c0
Alexandre Bailly <bobbycit2@gmail.com>
2023-08-29 17:31:57 +0200

    adds_controller

--- app/controllers/adds_controller.rb
+++ app/controllers/adds_controller.rb
@@ -1,2 +1,21 @@
 class AddsController < ApplicationController
+  def show
+    @add = Add.find(params[:id])
+    @image_url = fetch_random_image
+  end
+
+  private
+
+  def fetch_random_image
+    response = HTTParty.get('https://api.unsplash.com/photos/random', {
+    query: { client_id: 'KhA47ad3rLzB5NsxHPpkJjmpI5PSk4UCmMY4w9N_YRE' }
+    })
+
+    if response.success?
+      data = JSON.parse(response.body)
+      data['urls']['regular']
+    else
+      'https://example.com/default-image.jpg'
+    end
+  end
 end
