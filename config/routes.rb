fffb8c6c81b8aa59bf2811247c35a798e66153f3
Dimitri Kacimi Zlotnik <dimitri.zlotnik@gmail.com>
2023-08-29 12:09:35 +0200

    Ajout route resé

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :adds
-  resources :reservations, only: %i[ :new, :create, :edit, :delete]
+  resources :adds, only: [] do
+    resources :reservations, only: %i[new create edit delete]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
