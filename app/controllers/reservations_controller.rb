d213113e6c3c9f1459112d8396a2c30c13879e9c
Charles Deotto <charles.deotto@gmail.com>
2023-08-29 17:41:26 +0200

    Reservation New

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,8 @@
+class ReservationsController < ApplicationController
+
+  def new
+    @reservation = Reservation.new
+    @add = Add.find(params[:add_id])
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :adds, only: [] do
-    resources :reservations, only: %i[new create edit delete]
+  resources :adds  do
+    resources :reservations, only: [:new, :create]
   end
   # Defines the root path route ("/")
   # root "articles#index"
