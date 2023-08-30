df25b14322bea75487a65ee62d453f0ad6b9b406
Charles Deotto <charles.deotto@gmail.com>
2023-08-30 14:40:39 +0200

    Reservation New J2

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,29 @@
 class ReservationsController < ApplicationController
 
+  def index
+    @reservations = Reservation.where(user_id: current_user)
+  end
+
   def new
     @reservation = Reservation.new
     @add = Add.find(params[:add_id])
   end
-end
 
+  def create
+    @add = Add.find(params[:add_id])
+    @reservation = Reservation.new(reservation_params)
+    @reservation.add = @add
+    @reservation.user = current_user
+    if @reservation.save
+      redirect_to reservations_path
+    else
+      render 'reservations/new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out, :total_price, :add_id)
+  end
+end
---
+++ app/javascript/controllers/total_price_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+// const date_in = document.querySelector()
+// Connects to data-controller="total-price"
+export default class extends Controller {
+  connect() {
+
+  }
+}
