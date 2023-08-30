4dc1cef65c52c75ac65fb900a177a978211e985a
Dimitri Kacimi Zlotnik <dimitri.zlotnik@gmail.com>
2023-08-30 15:21:38 +0200

    Merge de la mort

--- app/models/add.rb
+++ app/models/add.rb
@@ -1,12 +1,8 @@
 class Add < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
 
   include PgSearch::Model
   pg_search_scope :search_by_city_and_title,
                   against: %I[city title],
                   using: { tsearch: { prefix: true } }
-=======
-  has_one_attached :photo
->>>>>>> 0bdd42eaaf3fc7d81a2c95ff07a966a84de9c709
 end
