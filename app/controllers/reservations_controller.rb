4ceafa76f62842d61e41d097176c28076a99d174
Alexandre Bailly <bobbycit2@gmail.com>
2023-08-30 15:03:44 +0200

    new

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,17 +1,13 @@
 class ReservationsController < ApplicationController
-<<<<<<< HEAD
 
   def index
     @reservations = Reservation.where(user_id: current_user)
   end
 
-=======
->>>>>>> 870005055aa49c2ec789b722e3ac2092b49b80a7
   def new
     @reservation = Reservation.new
     @add = Add.find(params[:add_id])
   end
-<<<<<<< HEAD
 
   def create
     @add = Add.find(params[:add_id])
@@ -30,6 +26,4 @@ class ReservationsController < ApplicationController
   def reservation_params
     params.require(:reservation).permit(:check_in, :check_out, :total_price, :add_id)
   end
-=======
->>>>>>> 870005055aa49c2ec789b722e3ac2092b49b80a7
 end
