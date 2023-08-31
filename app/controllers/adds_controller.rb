204389a27cf46ebdd9d61ef62152418e3e19a8ac
Charles Deotto <charles.deotto@gmail.com>
2023-08-31 13:25:15 +0200

    reservations new et add new

--- app/controllers/adds_controller.rb
+++ app/controllers/adds_controller.rb
@@ -1,7 +1,14 @@
 class AddsController < ApplicationController
+  def index
+  end
+
   def show
     @add = Add.find(params[:id])
     @image_url = fetch_random_image
+  end
+
+  def new
+    @add = Add.new
   end
 
   private
