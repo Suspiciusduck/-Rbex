85ba4cdf2af6c9cedbab0267ab3833181c2e927f
Dimitri Kacimi Zlotnik <dimitri.zlotnik@gmail.com>
2023-09-01 06:01:00 +0200

    Final push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,21 @@ class PagesController < ApplicationController
   end
 
   def home
-    @adds = Add.order(id: :asc)
+
+    if params[:query].present?
+      @adds = Add.search_by_city(params[:query])
+    else
+      @adds = Add.order(id: :asc)
+    end
+
+    cloudinary_folder = "Rbex"
+    folder_resources = Cloudinary::Api.resources(type: "upload", prefix: cloudinary_folder)
+    if folder_resources["resources"].empty?
+      # Gérer le cas où il n'y a pas d'images, par exemple en affichant un message d'erreur
+      @random_images = []
+    else
+      # Sélectionnez un nombre aléatoire d'images
+      @random_images = folder_resources["resources"].sample(10)
+    end
   end
 end
